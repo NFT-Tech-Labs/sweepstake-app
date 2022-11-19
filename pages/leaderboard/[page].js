776a74df214e969d341dd9d50b0b6b44f3c794b1
s-bilic <stefanbilic@hotmail.com>
2022-11-19 20:43:00 +0100

    Added fix for pagination

--- pages/leaderboard/[page].js
+++ pages/leaderboard/[page].js
@@ -1,9 +1,11 @@
-import styles from "../styles/leaderboard.module.scss";
+import styles from "../../styles/leaderboard.module.scss";
 import React, { useState, useEffect, useRef } from "react";
-import { Heading, Rank, Divider } from "@components";
-import { getData } from "../utils/api";
+import { Heading, Rank, Divider, Button } from "@components";
+import { getData } from "../../utils/api";
+import { useRouter } from "next/router";
 
-export default function Leaderboard() {
+export default function Leaderboard({ rankings, page }) {
+  const router = useRouter();
   const rankingsData = [
     {
       address: "ARLQYuL9HEoUtBXpDG26YyvGUAnHJfYbLSstvrm1vS24",
@@ -42,6 +44,7 @@ export default function Leaderboard() {
       points: 70,
     },
   ];
+
   return (
     <div className={styles.leaderboard}>
       <Heading
@@ -51,13 +54,13 @@ export default function Leaderboard() {
         }}
       />
       <Divider height={20} />
-      {rankingsData && (
+      {rankings && (
         <div className={styles.winners}>
-          {rankingsData.slice(0, 3)?.map((item, index) => (
+          {rankings?.slice(0, 3)?.map((item, index) => (
             <Rank
               key={index}
               position={`${index + 1}`}
-              points={"2"}
+              points={item?.totalPoints.toString()}
               label={"address"}
               winner
               className={styles.winner}
@@ -66,26 +69,58 @@ export default function Leaderboard() {
           ))}
         </div>
       )}
-
       <Divider height={20} />
-      {rankingsData?.slice(3, rankingsData.length)?.map((item, index) => (
+      {rankings?.slice(3, rankingsData.length)?.map((item, index) => (
         <Rank
           key={index}
+          points={item?.totalPoints.toString()}
           position={`${index + 4}`}
           label={"address"}
           {...item}
         />
       ))}
+      <Button type={"button"} onClick={() => router.push(page + 1)} />
     </div>
   );
 }
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  const offset = context.params.page;
+
   const rankings = await getData(
-    "https://backend-x7q2esrofa-no.a.run.app/api/v1/users"
+    `https://backend-x7q2esrofa-no.a.run.app/api/v1/users?order=ASC&limit=${10}&offset=${offset}`
   );
 
+  if (!rankings.length) {
+    return {
+      notFound: true,
+    };
+  }
+
   console.log(rankings);
+  console.log(offset);
+  console.log(context.params.page);
   return {
-    props: {},
+    props: {
+      rankings: rankings || null,
+      page: context.params.page,
+    },
+  };
+}
+
+export async function getStaticPaths(context) {
+  // Get total number of posts from API.
+
+  // Build paths `blog/0`, `blog/1` ...etc.
+  const paths = Array(10)
+    .fill(0)
+    .map((_, page) => ({
+      params: {
+        page: `${page + 1}`,
+      },
+    }));
+
+  return {
+    paths,
+    fallback: false,
   };
 }
